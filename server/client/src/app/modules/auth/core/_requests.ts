4c53c1146941692e7f22e8695cade22b0843c6bd
krishan191 <krishan.iresolve@gmail.com>
2025-02-06 17:58:14 +0530

    again add api/login commit

--- server/client/src/app/modules/auth/core/_requests.ts
+++ server/client/src/app/modules/auth/core/_requests.ts
@@ -7,9 +7,9 @@ const API_URL = import.meta.env.VITE_APP_API_URL;
 // export const LOGIN_URL = "/login";
 // export const REGISTER_URL = "/register";
 // export const REQUEST_PASSWORD_URL = "/forgot_password";
-export const GET_USER_BY_ACCESSTOKEN_URL = "/verify_token";
-export const LOGIN_URL = "/login";
-export const REGISTER_URL = "/register";
+export const GET_USER_BY_ACCESSTOKEN_URL = "api/verify_token";
+export const LOGIN_URL = "api/login";
+export const REGISTER_URL = "api/register";
 export const REQUEST_PASSWORD_URL = `${API_URL}/forgot_password`;
 
 // Server should return AuthModel
